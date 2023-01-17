6c332b71813fe952bcf9e24c774122cb06076fd9
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-17 14:11:14 +0200

    Task 2

--- src/Homework.java
+++ src/Homework.java
@@ -3,9 +3,16 @@ public class Homework {
     public static void main(String[] args) {
 
         byte clientOS = 0;
+        short clientDeviceYear = 2015;
 
         if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
