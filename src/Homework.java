fdd5d70d97804f32020c134ea61799975dc5d98e
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-17 13:51:05 +0200

    Task 1

--- src/Homework.java
+++ src/Homework.java
@@ -2,7 +2,13 @@ public class Homework {
 
     public static void main(String[] args) {
 
+        byte clientOS = 0;
 
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
     }
 
